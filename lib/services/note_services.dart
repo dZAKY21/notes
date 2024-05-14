bfe39831e077ee8c31d761d6fc8d6321c60bb91a
dZAKY21 <130964137+dZAKY21@users.noreply.github.com>
2024-05-14 08:53:04 +0700

    second push

---
+++ lib/services/note_services.dart
@@ -0,0 +1,56 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:notes/model/note.dart';
+
+class NoteService {
+  static final FirebaseFirestore _database = FirebaseFirestore.instance;
+  static final CollectionReference _notesCollection =
+      _database.collection('notes');
+
+  static Future<void> addNote(Note note) async {
+    Map<String, dynamic> newNote = {
+      'title': note.title,
+      'description': note.description,
+      'created_at': FieldValue.serverTimestamp(),
+      'updated_at': FieldValue.serverTimestamp(),
+    };
+    await _notesCollection.add(newNote);
+  }
+
+  static Future<void> updateNote(Note note) async {
+    Map<String, dynamic> updatedNote = {
+      'title': note.title,
+      'description': note.description,
+      'created_at': note.createdAt,
+      'updated_at': FieldValue.serverTimestamp(),
+    };
+
+    await _notesCollection.doc(note.id).update(updatedNote);
+  }
+
+  static Future<void> deleteNote(Note note) async {
+    await _notesCollection.doc(note.id).delete();
+  }
+
+  static Future<QuerySnapshot> retrieveNotes() {
+    return _notesCollection.get();
+  }
+
+  static Stream<List<Note>> getNoteList() {
+    return _notesCollection.snapshots().map((snapshot) {
+      return snapshot.docs.map((doc) {
+        Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+        return Note(
+          id: doc.id,
+          title: data['title'],
+          description: data['description'],
+          createdAt: data['created_at'] != null
+              ? data['created_at'] as Timestamp
+              : null,
+          updatedAt: data['updated_at'] != null
+              ? data['updated_at'] as Timestamp
+              : null,
+        );
+      }).toList();
+    });
+  }
+}
